231ae642926bb3777635cfc057028a984f1374ef
sp932 <sapanpatel@Sapans-MacBook-Pro.local>
2016-12-07 21:30:16 -0500

    logout/kill session and removed copy/pdf/csv buttons

--- class-registration/registration.php
+++ class-registration/registration.php
@@ -27,24 +27,38 @@
 <script>
  $(document).ready( function () {
     $("#myTable").dataTable( {
-        "dom": 'T<"clear">f',
+        "dom": '<"clear">f',
         "paging": false,
-        "tableTools": {
-            "sSwfPath": "swf/copy_csv_xls_pdf.swf"
-        }
+        "searching": true,
     } );
 } );
  </script>
 
 
-
-
-
-
 </head>
 
 <body>
+  <nav class="navbar navbar-default" role="navigation">
+    <div class="container-fluid">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+      </div>
 
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="registration.php">Course Registration<span class="sr-only">(current)</span></a></li>
+          <li><a href="logout.php">Logout</a></li>
+        </ul>
+      </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+  </nav>
 
 
     <div class="container">
